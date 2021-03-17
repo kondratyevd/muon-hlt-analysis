bcf7e0992c2cd834eb6aff33a7af13b7e44555e4
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2021-03-17 03:34:16 -0400

    standartize names and add more datasets

---
+++ datasets/datasets_phase2_DY_L2vsL1TkMu.py
@@ -0,0 +1,54 @@
+phase2_pu140_OIFromL2 = {
+            'name':'PU 140: OI from L2',
+            'path':'/mnt/hadoop/store/user/dkondrat/muonHLT_phase2_DYToLL_PU140_OIFromL2_default/DYToLL_M-50_TuneCP5_14TeV-pythia8/muonHLT_phase2_DYToLL_PU140_OIFromL2_default/210317_010510/0000/muon*root',
+        }
+
+phase2_pu140_OIFromL1TkMu = {
+            'name':'PU 140: OI from L1TkMu',
+            'path':'/mnt/hadoop/store/user/dkondrat/muonHLT_phase2_DYToLL_PU140_OIFromL1TkMu_default/DYToLL_M-50_TuneCP5_14TeV-pythia8/muonHLT_phase2_DYToLL_PU140_OIFromL1TkMu_default/210317_073058/0000/muon*root',
+            'collection': 'L1Tkmuons'
+        }
+
+phase2_pu140_OIFromL2_VH = {
+            'name':'PU 140: OI from L2 - VectorHits',
+            'path':'/mnt/hadoop/store/user/dkondrat/muonHLT_phase2_DYToLL_PU140_OIFromL2_default_VHenabled/DYToLL_M-50_TuneCP5_14TeV-pythia8/muonHLT_phase2_DYToLL_PU140_OIFromL2_default_VHenabled/210317_015040/0000/muon*root'
+        }
+
+phase2_pu140_OIFromL1TkMu_VH = {
+            'name':'PU 140: OI from L1TkMu - VectorHits',
+            'path':'/mnt/hadoop/store/user/dkondrat/muonHLT_phase2_DYToLL_PU140_OIFromL1TkMu_default_VHenabled/DYToLL_M-50_TuneCP5_14TeV-pythia8/muonHLT_phase2_DYToLL_PU140_OIFromL1TkMu_default_VHenabled/210317_073126/0000/muon*root',
+            'collection': 'L1Tkmuons'
+        }
+
+
+phase2_noPU_OIFromL2 = {
+            'name':'no PU: OI from L2',
+            'path':'/mnt/hadoop/store/user/dkondrat/muonHLT_phase2_DYToLL_noPU_OIFromL2_default/DYToLL_M-50_TuneCP5_14TeV-pythia8/muonHLT_phase2_DYToLL_noPU_OIFromL2_default/210317_033103/0000/muon*root',
+        }
+
+phase2_noPU_OIFromL1TkMu = {
+            'name':'no PU: OI from L1TkMu',
+            'path':'/mnt/hadoop/store/user/dkondrat/muonHLT_phase2_DYToLL_noPU_OIFromL1TkMu_default/DYToLL_M-50_TuneCP5_14TeV-pythia8/muonHLT_phase2_DYToLL_noPU_OIFromL1TkMu_default/210317_062447/0000/muon*root',
+            'collection': 'L1Tkmuons'
+        }
+
+phase2_noPU_OIFromL2_VH = {
+            'name':'no PU: OI from L2 - VectorHits',
+            'path':'/mnt/hadoop/store/user/dkondrat/muonHLT_phase2_DYToLL_noPU_OIFromL2_default_VHenabled/DYToLL_M-50_TuneCP5_14TeV-pythia8/muonHLT_phase2_DYToLL_noPU_OIFromL2_default_VHenabled/210317_033132/0000/muon*root'
+        }
+phase2_noPU_OIFromL1TkMu_VH = {
+            'name':'no PU: OI from L1TkMu - VectorHits',
+            'path':'/mnt/hadoop/store/user/dkondrat/muonHLT_phase2_DYToLL_noPU_OIFromL1TkMu_default_VHenabled/DYToLL_M-50_TuneCP5_14TeV-pythia8/muonHLT_phase2_DYToLL_noPU_OIFromL1TkMu_default_VHenabled/210317_062517/0000/muon*root',
+            'collection': 'L1Tkmuons'
+        }
+
+datasets = [
+    #phase2_noPU_OIFromL2,
+    #phase2_noPU_OIFromL2_VH,
+    #phase2_noPU_OIFromL1TkMu,
+    #phase2_noPU_OIFromL1TkMu_VH,
+    phase2_pu140_OIFromL2,
+    phase2_pu140_OIFromL2_VH,
+    phase2_pu140_OIFromL1TkMu,
+    phase2_pu140_OIFromL1TkMu_VH,
+]
